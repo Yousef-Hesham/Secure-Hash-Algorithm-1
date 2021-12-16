08a5d7f07911e5d45d88da939cd7d8687ad1c55c
Yousef Hesham <Yousef.hesham.samir@gmail.com>
2021-12-16 11:57:02 +0200

    Initial Version

---
+++ SHA-1/SHA-1.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include <iostream>
+
+#define BLOCK_INTS 16  /* number of 32bit integers per SHA1 block */
+#define BLOCK_BYTES (BLOCK_INTS*4)
+
+class sha1 {
+public:
+	sha1();
+	~sha1();
+
+    void sha1::add_message(const std::string& s);
+    std::string sha1::get_sha1();
+
+private:
+    uint32_t digest[5];
+    std::string buffer;
+    uint64_t transforms_num;
+
+
+    void sha1::reset();
+    uint32_t sha1::rol(const uint32_t value, const size_t bits);
+    uint32_t sha1::blk(const uint32_t block[BLOCK_INTS], const size_t i);
+    void sha1::buffer_to_block(const std::string& buffer, uint32_t block[BLOCK_INTS]);
+    void sha1::R0(const uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t& w, const uint32_t x, const uint32_t y, uint32_t& z, const size_t i);
+    void sha1::R1(uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t& w, const uint32_t x, const uint32_t y, uint32_t& z, const size_t i);
+    void sha1::R2(uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t& w, const uint32_t x, const uint32_t y, uint32_t& z, const size_t i);
+    void sha1::R3(uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t& w, const uint32_t x, const uint32_t y, uint32_t& z, const size_t i);
+    void sha1::R4(uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t& w, const uint32_t x, const uint32_t y, uint32_t& z, const size_t i);
+    void sha1::perform_sha1(uint32_t digest[], uint32_t block[BLOCK_INTS], uint64_t& transforms);
+};
---
+++ SHA-1/main.cpp
@@ -0,0 +1,41 @@
+﻿/*
+Authors : Yousef Hesham
+		  Youssef Ansara
+		  Eman Mohammed
+*/
+#include "SHA-1.h"
+
+using namespace std;
+
+
+void compare(const string& result, const string& expected)
+{
+	const string& state = (result == expected) ? "OK" : "Failure";
+	cout << "Result:   " << result << endl;
+	cout << "Expected: " << expected << "  (" << state << ")" << endl;
+}
+
+int main()
+{
+	cout << "Hello SHA1." << endl;
+	sha1 instance;
+
+    /* Test Vectors */
+    cout << endl;
+    cout << "Test:     abc" << endl;
+    instance.add_message("abc");
+    compare(instance.get_sha1(), "a9993e364706816aba3e25717850c26c9cd0d89d");
+
+    cout << endl;
+    cout << "Test:     abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq" << endl;
+    instance.add_message("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+    compare(instance.get_sha1(), "84983e441c3bd26ebaae4aa1f95129e5e54670f1");
+
+
+    cout << endl;
+    cout << "Test:     abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu" << endl;
+    instance.add_message("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
+    compare(instance.get_sha1(), "a49b2446a02c645bf419f995b67091253a04a259");
+
+	return 0;
+}
